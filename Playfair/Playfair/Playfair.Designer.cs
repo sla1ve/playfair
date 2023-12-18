f268514383123e85117b3fd449c6703b55ac4af4
sla1ve <106611178+sla1ve@users.noreply.github.com>
2023-12-18 15:34:57 +0700

    Add files via upload

---
+++ Playfair/Playfair/Playfair.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace Playfair
+{
+    partial class Playfair
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            textBox15 = new TextBox();
+            textBox16 = new TextBox();
+            textBox17 = new TextBox();
+            textBox18 = new TextBox();
+            textBox19 = new TextBox();
+            textBox20 = new TextBox();
+            textBox21 = new TextBox();
+            textBox22 = new TextBox();
+            textBox23 = new TextBox();
+            textBox24 = new TextBox();
+            textBox25 = new TextBox();
+            label1 = new Label();
+            btnEncrypt = new Button();
+            txtPlaintext = new TextBox();
+            txtCiphertext = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            btnGenerate = new Button();
+            label4 = new Label();
+            txtKeyword = new TextBox();
+            btnDecrypt = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(30, 91);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(30, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(89, 91);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(30, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(144, 91);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(30, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(202, 91);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(30, 27);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(263, 91);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(30, 27);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(30, 149);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(30, 27);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(89, 149);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(30, 27);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(144, 149);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(30, 27);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(202, 149);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(30, 27);
+            textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(263, 149);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(30, 27);
+            textBox10.TabIndex = 9;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(30, 210);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(30, 27);
+            textBox11.TabIndex = 10;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(89, 210);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(30, 27);
+            textBox12.TabIndex = 11;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(144, 210);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(30, 27);
+            textBox13.TabIndex = 12;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(202, 210);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(30, 27);
+            textBox14.TabIndex = 13;
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(263, 210);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(30, 27);
+            textBox15.TabIndex = 14;
+            // 
+            // textBox16
+            // 
+            textBox16.Location = new Point(30, 269);
+            textBox16.Name = "textBox16";
+            textBox16.Size = new Size(30, 27);
+            textBox16.TabIndex = 15;
+            // 
+            // textBox17
+            // 
+            textBox17.Location = new Point(89, 269);
+            textBox17.Name = "textBox17";
+            textBox17.Size = new Size(30, 27);
+            textBox17.TabIndex = 16;
+            // 
+            // textBox18
+            // 
+            textBox18.Location = new Point(144, 269);
+            textBox18.Name = "textBox18";
+            textBox18.Size = new Size(30, 27);
+            textBox18.TabIndex = 17;
+            // 
+            // textBox19
+            // 
+            textBox19.Location = new Point(202, 269);
+            textBox19.Name = "textBox19";
+            textBox19.Size = new Size(30, 27);
+            textBox19.TabIndex = 18;
+            // 
+            // textBox20
+            // 
+            textBox20.Location = new Point(263, 269);
+            textBox20.Name = "textBox20";
+            textBox20.Size = new Size(30, 27);
+            textBox20.TabIndex = 19;
+            // 
+            // textBox21
+            // 
+            textBox21.Location = new Point(30, 328);
+            textBox21.Name = "textBox21";
+            textBox21.Size = new Size(30, 27);
+            textBox21.TabIndex = 20;
+            // 
+            // textBox22
+            // 
+            textBox22.Location = new Point(89, 328);
+            textBox22.Name = "textBox22";
+            textBox22.Size = new Size(30, 27);
+            textBox22.TabIndex = 21;
+            // 
+            // textBox23
+            // 
+            textBox23.Location = new Point(144, 328);
+            textBox23.Name = "textBox23";
+            textBox23.Size = new Size(30, 27);
+            textBox23.TabIndex = 22;
+            // 
+            // textBox24
+            // 
+            textBox24.Location = new Point(202, 328);
+            textBox24.Name = "textBox24";
+            textBox24.Size = new Size(30, 27);
+            textBox24.TabIndex = 23;
+            // 
+            // textBox25
+            // 
+            textBox25.Location = new Point(263, 328);
+            textBox25.Name = "textBox25";
+            textBox25.Size = new Size(30, 27);
+            textBox25.TabIndex = 24;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 20);
+            label1.TabIndex = 25;
+            label1.Text = "Ma trận";
+            // 
+            // btnEncrypt
+            // 
+            btnEncrypt.Location = new Point(511, 172);
+            btnEncrypt.Name = "btnEncrypt";
+            btnEncrypt.Size = new Size(106, 29);
+            btnEncrypt.TabIndex = 26;
+            btnEncrypt.Text = "Encrypt >>";
+            btnEncrypt.UseVisualStyleBackColor = true;
+            btnEncrypt.Click += btnEncrypt_Click;
+            // 
+            // txtPlaintext
+            // 
+            txtPlaintext.Location = new Point(342, 91);
+            txtPlaintext.Multiline = true;
+            txtPlaintext.Name = "txtPlaintext";
+            txtPlaintext.Size = new Size(163, 264);
+            txtPlaintext.TabIndex = 28;
+            // 
+            // txtCiphertext
+            // 
+            txtCiphertext.Location = new Point(625, 91);
+            txtCiphertext.Multiline = true;
+            txtCiphertext.Name = "txtCiphertext";
+            txtCiphertext.Size = new Size(163, 264);
+            txtCiphertext.TabIndex = 29;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(389, 50);
+            label2.Name = "label2";
+            label2.Size = new Size(66, 20);
+            label2.TabIndex = 30;
+            label2.Text = "Plaintext";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(669, 50);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 31;
+            label3.Text = "Ciphertext";
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(181, 46);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(112, 29);
+            btnGenerate.TabIndex = 32;
+            btnGenerate.Text = "Generate";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(345, 381);
+            label4.Name = "label4";
+            label4.Size = new Size(118, 20);
+            label4.TabIndex = 33;
+            label4.Text = "Playfair keyword";
+            // 
+            // txtKeyword
+            // 
+            txtKeyword.Location = new Point(469, 378);
+            txtKeyword.Name = "txtKeyword";
+            txtKeyword.Size = new Size(319, 27);
+            txtKeyword.TabIndex = 34;
+            // 
+            // btnDecrypt
+            // 
+            btnDecrypt.Location = new Point(513, 223);
+            btnDecrypt.Name = "btnDecrypt";
+            btnDecrypt.Size = new Size(106, 27);
+            btnDecrypt.TabIndex = 35;
+            btnDecrypt.Text = "<< Decrypt";
+            btnDecrypt.UseVisualStyleBackColor = true;
+            btnDecrypt.Click += btnDecrypt_Click;
+            // 
+            // Playfair
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDecrypt);
+            Controls.Add(txtKeyword);
+            Controls.Add(label4);
+            Controls.Add(btnGenerate);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtCiphertext);
+            Controls.Add(txtPlaintext);
+            Controls.Add(btnEncrypt);
+            Controls.Add(label1);
+            Controls.Add(textBox25);
+            Controls.Add(textBox24);
+            Controls.Add(textBox23);
+            Controls.Add(textBox22);
+            Controls.Add(textBox21);
+            Controls.Add(textBox20);
+            Controls.Add(textBox19);
+            Controls.Add(textBox18);
+            Controls.Add(textBox17);
+            Controls.Add(textBox16);
+            Controls.Add(textBox15);
+            Controls.Add(textBox14);
+            Controls.Add(textBox13);
+            Controls.Add(textBox12);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Playfair";
+            Text = "Playfair";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private TextBox textBox13;
+        private TextBox textBox14;
+        private TextBox textBox15;
+        private TextBox textBox16;
+        private TextBox textBox17;
+        private TextBox textBox18;
+        private TextBox textBox19;
+        private TextBox textBox20;
+        private TextBox textBox21;
+        private TextBox textBox22;
+        private TextBox textBox23;
+        private TextBox textBox24;
+        private TextBox textBox25;
+        private Label label1;
+        private Button btnEncrypt;
+        private TextBox txtPlaintext;
+        private TextBox txtCiphertext;
+        private Label label2;
+        private Label label3;
+        private Button btnGenerate;
+        private Label label4;
+        private TextBox txtKeyword;
+        private Button btnDecrypt;
+    }
+}
---
+++ Playfair/Playfair/Playfair.cs
@@ -0,0 +1,294 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Playfair
+{
+    public partial class Playfair : Form
+    {
+        public Playfair()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private char[,] GeneratePlayfairMatrix(List<char> characters)
+        {
+            // Tạo một ma trận 5x5 để sử dụng trong thuật toán mã hóa Playfair
+            char[,] matrix = new char[5, 5];
+            int index = 0;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    // Loại bỏ ký tự 'J' nếu có
+                    while (characters[index] == 'J')
+                    {
+                        index++;
+                    }
+
+                    matrix[row, col] = characters[index++];
+                }
+            }
+
+            return matrix;
+        }
+
+        private void DisplayMatrix(char[,] matrix)
+        {
+            // Hiển thị ma trận trong các TextBox từ TextBox1 đến TextBox25
+            int index = 0;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+
+                    TextBox textBox = Controls.Find($"textBox{index + 1}", true).FirstOrDefault() as TextBox;
+
+                    if (textBox != null)
+                    {
+                        // Hiển thị giá trị từ ma trận vào TextBox
+                        textBox.Text = matrix[row, col].ToString();
+                        index++;
+                    }
+                }
+            }
+        }
+
+        private List<char> ProcessKeyword(string keyword)
+        {
+
+            // Loại bỏ ký tự 'J' nếu có
+            keyword = keyword.Replace("J", "");
+
+            // Loại bỏ ký tự khoảng trắng
+            keyword = keyword.Replace(" ", "");
+
+            // Loại bỏ các ký tự trùng lặp trong từ khóa
+            string uniqueChars = new string(keyword.Distinct().ToArray());
+
+            // Tạo danh sách ký tự từ từ khóa và bổ sung các ký tự còn lại của bảng chữ cái
+            List<char> characters = uniqueChars.ToList();
+            characters.AddRange(Enumerable.Range('A', 26).Select(c => (char)c).Except(uniqueChars));
+
+            return characters;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            // Lấy từ khóa từ txtKeyword
+            string keyword = txtKeyword.Text.ToUpper();
+
+            // Xử lý từ khóa để tạo danh sách ký tự cho ma trận Playfair
+            List<char> characters = ProcessKeyword(keyword);
+
+            // Tạo ma trận Playfair từ danh sách ký tự đã xử lý
+            char[,] playfairMatrix = GeneratePlayfairMatrix(characters);
+
+            // Hiển thị ma trận trong các TextBox từ TextBox1 đến TextBox25
+            DisplayMatrix(playfairMatrix);
+        }
+
+        private char[,] GetPlayfairMatrixFromTextBoxes()
+        {
+            // Tạo ma trận 5x5 từ TextBoxes
+            char[,] matrix = new char[5, 5];
+            int index = 0;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    // Tìm TextBox theo tên
+                    TextBox textBox = Controls.Find($"textBox{index + 1}", true).FirstOrDefault() as TextBox;
+
+                    if (textBox != null)
+                    {
+                        // Lấy giá trị từ TextBox và đặt vào ma trận
+                        matrix[row, col] = textBox.Text[0];
+                        index++;
+                    }
+                }
+            }
+
+            return matrix;
+        }
+
+        private Tuple<int, int> FindCharPosition(char[,] playfairMatrix, char target)
+        {
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    if (playfairMatrix[row, col] == target)
+                    {
+                        return Tuple.Create(row, col);
+                    }
+                }
+            }
+
+            // Trả về giá trị mặc định nếu không tìm thấy
+            return Tuple.Create(-1, -1);
+        }
+
+        private string ProcessInput(string input)
+        {
+            // Chuẩn hóa văn bản đầu vào
+            input = input.ToUpper().Replace("J", "I");
+            input = Regex.Replace(input, "[^A-Z]", "");
+
+            // Thêm 'X' giữa các cặp ký tự giống nhau hoặc giữa ký tự cuối cùng nếu số ký tự là lẻ
+            for (int i = 0; i < input.Length - 1; i += 2)
+            {
+                if (input[i] == input[i + 1])
+                {
+                    input = input.Insert(i + 1, "X");
+                }
+            }
+
+            if (input.Length % 2 != 0)
+            {
+                input += "X";
+            }
+
+            return input;
+        }
+
+        private string PlayfairEncrypt(string plaintext, char[,] playfairMatrix)
+        {
+            // Chuẩn hóa văn bản đầu vào
+            plaintext = ProcessInput(plaintext);
+
+            StringBuilder ciphertext = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length - 1; i += 2)
+            {
+                char char1 = plaintext[i];
+                char char2 = plaintext[i + 1];
+
+                // Tìm vị trí của các ký tự trong ma trận Playfair
+                Tuple<int, int> pos1 = FindCharPosition(playfairMatrix, char1);
+                Tuple<int, int> pos2 = FindCharPosition(playfairMatrix, char2);
+
+                if (pos1.Item1 == pos2.Item1) // Cùng hàng
+                {
+                    ciphertext.Append(playfairMatrix[pos1.Item1, (pos1.Item2 + 1) % 5]);
+                    ciphertext.Append(playfairMatrix[pos2.Item1, (pos2.Item2 + 1) % 5]);
+                }
+                else if (pos1.Item2 == pos2.Item2) // Cùng cột
+                {
+                    ciphertext.Append(playfairMatrix[(pos1.Item1 + 1) % 5, pos1.Item2]);
+                    ciphertext.Append(playfairMatrix[(pos2.Item1 + 1) % 5, pos2.Item2]);
+                }
+                else // Tạo hình chữ nhật
+                {
+                    ciphertext.Append(playfairMatrix[pos1.Item1, pos2.Item2]);
+                    ciphertext.Append(playfairMatrix[pos2.Item1, pos1.Item2]);
+                }
+            }
+
+            return ciphertext.ToString();
+        }
+
+        private string PlayfairDecrypt(string ciphertext, char[,] playfairMatrix)
+        {
+            // Chuẩn hóa văn bản đầu vào
+            ciphertext = ProcessInput(ciphertext);
+
+            StringBuilder plaintext = new StringBuilder();
+
+            for (int i = 0; i < ciphertext.Length - 1; i += 2)
+            {
+                char char1 = ciphertext[i];
+                char char2 = ciphertext[i + 1];
+
+                // Tìm vị trí của các ký tự trong ma trận Playfair
+                Tuple<int, int> pos1 = FindCharPosition(playfairMatrix, char1);
+                Tuple<int, int> pos2 = FindCharPosition(playfairMatrix, char2);
+
+                if (pos1.Item1 == pos2.Item1) // Cùng hàng
+                {
+                    plaintext.Append(playfairMatrix[pos1.Item1, (pos1.Item2 - 1 + 5) % 5]);
+                    plaintext.Append(playfairMatrix[pos2.Item1, (pos2.Item2 - 1 + 5) % 5]);
+                }
+                else if (pos1.Item2 == pos2.Item2) // Cùng cột
+                {
+                    plaintext.Append(playfairMatrix[(pos1.Item1 - 1 + 5) % 5, pos1.Item2]);
+                    plaintext.Append(playfairMatrix[(pos2.Item1 - 1 + 5) % 5, pos2.Item2]);
+                }
+                else // Tạo hình chữ nhật
+                {
+                    plaintext.Append(playfairMatrix[pos1.Item1, pos2.Item2]);
+                    plaintext.Append(playfairMatrix[pos2.Item1, pos1.Item2]);
+                }
+            }
+
+            return plaintext.ToString();
+        }
+
+        private bool AreTextBoxesFilled()
+        {
+            // Kiểm tra xem tất cả TextBox từ textBox1 đến textBox25 có giá trị hay không
+            for (int i = 1; i <= 25; i++)
+            {
+                TextBox textBox = Controls.Find($"textBox{i}", true).FirstOrDefault() as TextBox;
+
+                if (textBox == null || string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    return false; // Có ít nhất một TextBox chưa được điền giá trị
+                }
+            }
+
+            return true; // Tất cả TextBox đã được điền giá trị
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem ma trận Playfair đã được tạo chưa
+            if (!AreTextBoxesFilled())
+            {
+                MessageBox.Show("Hãy ấn vào nút Generate để tạo ma trận.", "Ma trận chưa được tạo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Stop further execution
+            }
+
+            // Lấy văn bản gốc từ txtPlaintext
+            string plaintext = txtPlaintext.Text.ToUpper();
+
+            // Lấy ma trận Playfair từ TextBoxes
+            char[,] playfairMatrix = GetPlayfairMatrixFromTextBoxes();
+
+            // Mã hóa văn bản
+            string ciphertext = PlayfairEncrypt(plaintext, playfairMatrix);
+
+            // Hiển thị văn bản đã mã hóa trong txtCiphertext
+            txtCiphertext.Text = ciphertext;
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem ma trận Playfair đã được tạo chưa
+            if (!AreTextBoxesFilled())
+            {
+                MessageBox.Show("Hãy ấn vào nút Generate để tạo ma trận.", "Ma trận chưa được tạo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Stop further execution
+            }
+
+            // Lấy văn bản gốc từ txtCiphertext
+            string ciphertext = txtCiphertext.Text.ToUpper();
+
+            // Lấy ma trận Playfair từ TextBoxes
+            char[,] playfairMatrix = GetPlayfairMatrixFromTextBoxes();
+
+            // Giải mã văn bản
+            string plaintext = PlayfairDecrypt(ciphertext, playfairMatrix);
+
+            // Hiển thị văn bản đã giải mã trong txtCiphertext
+            txtPlaintext.Text = plaintext;
+        }
+    }
+}
